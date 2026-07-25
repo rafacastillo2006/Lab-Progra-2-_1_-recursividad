98f3909da1623d9b9a5be046007e36c325c9f306
JuanCarlosxd01 <juan_alcerro@unitec.edu>
2026-07-25 11:14:49 -0600

    Actualizar

--- LabProgramacion2_1_Recursividad/src/labprogramacion2_1_recursividad/palindromo.java
+++ LabProgramacion2_1_Recursividad/src/labprogramacion2_1_recursividad/palindromo.java
@@ -5,7 +5,18 @@ package labprogramacion2_1_recursividad;
 public class palindromo {
     
     
-    
+    public String sellTicket(String nombre){
+        int asiento = firstAvailable(0);
+        boolean palindromo = esPalindromo(nombre);
+        
+        if(asiento == -1){
+            return "No has asientos disponibles en este momento, vuelva pronto";  
+        }
+        
+        asientos[asiento] = new Ticket(nombre, Precio_Base, palindromo);
+        
+        return "Ticket vendido.\n" + "Asiento: " + (asiento + 1) + "\n Monto pagado: L." + asientos[asiento].getFinalAmount();
+    }
     
     public static void cancelTicket(String nombre){
         
