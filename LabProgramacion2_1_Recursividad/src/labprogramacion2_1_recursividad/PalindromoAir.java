cd8a7059aae1d61ce357fae6b090a62bbedd044d
JuanCarlosxd01 <juan_alcerro@unitec.edu>
2026-07-25 10:48:51 -0600

    Actualizar palindromoAir

--- LabProgramacion2_1_Recursividad/src/labprogramacion2_1_recursividad/PalindromoAir.java
+++ LabProgramacion2_1_Recursividad/src/labprogramacion2_1_recursividad/PalindromoAir.java
@@ -30,6 +30,52 @@ public class PalindromoAir {
            }
         }
         
+        private boolean esPalindromo(String nombre, int prim, int seg){
+        if(prim<seg){
+            if(nombre.charAt(prim)== nombre.charAt(seg)){
+               return esPalindromo(nombre, prim+1, seg-1); 
+        }
+            return false;
+    }
+        return true;
+  }
+    
+    public boolean esPalindromo(String nombre){
+        nombre = nombre.toLowerCase().replace(" ", "");
+        return esPalindromo(nombre, 0, nombre.length() - 1);
+    }
+        
+        public String printPassengers(int index){
+        if(index == asientos.length){
+            return "";
+        }
+        if(asientos[index] == null){
+            return printPassengers(index + 1);
+        }
+        
+        return "Asiento" + (index + 1) + "\n" + asientos[index].imprimirInformacion() + "\n\n" + printPassengers(index + 1);
+    }
+    
+    public double income(int index){
+        if (index == asientos.length){
+            return 0;
+        }
+        
+        if(asientos[index] == null){
+            return income(index+1);
+        }
+        
+        return asientos[index].getFinalAmount() + income(index + 1);
+    }
+    
+    public void reset(int index){
+        if (index == asientos.length){
+            return;
+        }
+        
+        asientos[index] = null;
+    }
+        
         
     }
 }
